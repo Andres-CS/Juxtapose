5cef441c604c7d15de51954dc409cccca1593c65
Andy <softwareapg@gmail.com>
2020-09-09 02:42:03 -0400

    Created method to get PDF file info and deleted dummy prints

--- src/Application.java
+++ src/Application.java
@@ -1,15 +1,15 @@
-//import pdfFiles;
+import java.io.IOException;
 
 public class Application {
 
-	public static void main(String[] args)
-	{	
+	public static void main(String[] args) throws IOException {	
 		System.out.println("TESTING");
 		
 		pdfFiles var = new pdfFiles();
 		
-		var.setFilename("somefile.pdf");
+		var.setFilename("C:\\Users\\18628\\Desktop\\Development\\TEST_Files\\002272__DBJPM 2020-C9\\Preliminary\\PPM\\DBJPM 2020-C9 - PPM__MERGED.pdf");
 		
+		var.getPDFinfo();
 
 	}
 
--- src/pdfFiles.java
+++ src/pdfFiles.java
@@ -1,6 +1,12 @@
 
 import java.io.File;
+import java.io.IOException;
+
+import java.util.Calendar;
+
 import java.lang.String;
+
+import java.text.SimpleDateFormat;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDDocumentInformation;
@@ -8,22 +14,26 @@ import org.apache.pdfbox.pdmodel.PDDocumentInformation;
 public class pdfFiles {
 	
 	private String FName;
+	private File pathFile; 
 	
 	
-	//Constructors
-	public pdfFiles() {
-		//Default Constructor;
-	}
+	/*
+	  -- Constructors -- 
+	*/
+	public pdfFiles() {	}
 	
 	public pdfFiles(String FilePath) {
-		this.FName = FilePath;
-		System.out.println("File to work on is: " + this.FName);
-		
 		if(this.hasdotpdf(FilePath)) {
-			System.out.println("GOOD TO GO.");
+			this.FName = FilePath;
+			//Create File obj
+			pathFile = new File(FilePath);
+			System.out.println("File to work on is: " + this.FName);
 		}
-		
 	}
+	
+	/*
+	  -- Checkers -- 
+	*/
 	
 	//Check file has correct File extension
 	private boolean hasdotpdf(String FilePath) {
@@ -39,11 +49,52 @@ public class pdfFiles {
 	}
 	
 	
-	//Setters
+	/*
+	  -- Setters -- 
+	*/
 	public void setFilename(String FilePath) {
 		this.FName = FilePath;
 		if(this.hasdotpdf(FilePath)) {
 			System.out.println("GOOD TO GO.");
+		}
+	}
+	
+	
+	/*
+	  -- Getters -- 
+	*/
+	
+	public void getPDFinfo() throws IOException{
+	
+		try{
+			System.out.println("getPDFinfo try");
+			PDDocument document = PDDocument.load(new File(this.FName));
+			PDDocumentInformation info = document.getDocumentInformation();
+			
+			//Retrieving the info of a PDF document
+		    System.out.println("Author of the document is :"+ info.getAuthor());
+		    System.out.println("Title of the document is :"+ info.getTitle());
+		    System.out.println("Subject of the document is :"+ info.getSubject());
+	
+		    System.out.println("Creator of the document is :"+ info.getCreator());
+		    
+		    Calendar creationDate = info.getCreationDate();
+		    SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd");
+		    String formatted = format1.format(creationDate.getTime());
+		    
+		    System.out.println("Creation date of the document is :"+ formatted);
+		    
+		    Calendar modifDate = info.getModificationDate();
+		    formatted = format1.format(modifDate.getTime());
+		    
+		    System.out.println("Modification date of the document is :"+ formatted); // info.getModificationDate()); 
+		    System.out.println("Keywords of the document are :"+ info.getKeywords()); 
+		     
+		    //Closing the document 
+		    document.close();
+		}
+		catch (IOException e) {
+			System.out.println("Something happend" + e);
 		}
 	}
 	
